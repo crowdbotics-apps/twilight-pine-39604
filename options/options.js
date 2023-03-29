03ad140afbb09a26dc1251ca7a01695b4e25ce35
Crowdbotics <team@crowdbotics.com>
2023-03-29 08:47:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-pine-39604",
+  url: "https://twilight-pine-39604.botics.co",
+  api: "https://twilight-pine-39604.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
